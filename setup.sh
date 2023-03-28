d2b2032e49a10bb57eb8b12ea51e31c82772abec
Jean Brigonnet <jeanbrigo@gmail.com>
2023-03-28 11:42:42 -0500

    added password reset dependency

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ set -o errexit
 ## Install dependencies
 pip install -r dependencies.txt
 pip install django-rest-knox
+pip install django_rest_passwordreset
 
 
 ## Run migrations in case any migrations hadn't been run yet
