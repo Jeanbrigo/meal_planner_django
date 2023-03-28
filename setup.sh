0f5222c0c160cc578cc3d1c56d6ed2a30a9d37c7
Jean Brigonnet <jeanbrigo@gmail.com>
2023-03-28 11:40:00 -0500

    added install dependency

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ set -o errexit
 
 ## Install dependencies
 pip install -r dependencies.txt
+pip install django-rest-knox
+
 
 ## Run migrations in case any migrations hadn't been run yet
 python manage.py migrate
